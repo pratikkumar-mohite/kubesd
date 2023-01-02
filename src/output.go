bbf930092f233bb4b4fa5a46863caee8e351994d
Pratikkumar Mohite <mohite770.pm@gmail.com>
2023-01-02 21:30:50 +0530

    Added json object support

--- src/output.go
+++ src/output.go
@@ -4,17 +4,24 @@ import (
 	"fmt"
 
 	yaml "gopkg.in/yaml.v2"
+	json "encoding/json"
 )
 
 const dataObject = "data"
 
-func printSecretObject(yData *secretData, yComplete Secret){
+func printSecretObject(yData *secretData, yComplete SecretYaml){
+	var object []uint8
+	var err error
 	for key := range yComplete {
 		if key == dataObject {
 			yComplete[key] = yData.Data
 		}
 	}
-	object, err := yaml.Marshal(yComplete)
+	if OutputType == "json" {
+		object, err = json.MarshalIndent(yComplete,"","\t")
+	} else {
+		object, err = yaml.Marshal(yComplete)
+	}
 	if err != nil {
 		fmt.Printf("Failed to encode the secret object while printing %v\n",err)
 	}
--- src/parser.go
+++ src/parser.go
@@ -7,20 +7,23 @@ import (
 	"strings"
 
 	yaml "gopkg.in/yaml.v2"
+	json "encoding/json"
 )
 
 type secretData struct {
-	Kind string `yaml:"kind"`
-	Type string `yaml:"type"`
-	Data map[string]string `yaml:"data"`
+	Kind string `yaml:"kind" json:"kind"`
+	Type string `yaml:"type" json:"type"`
+	Data map[string]string `yaml:"data" json:"data"`
 }
 
-type Secret map[string]interface{}
+type SecretYaml map[string]interface{}
+
+var OutputType string
 
 // Read secret object from stdin
-func readObject() (yData *secretData, yComplete Secret, objectType string){
-	var yamlData secretData
-	var yamlComplete Secret
+func readObject() (yData *secretData, yComplete SecretYaml, objectType string){
+	var secretDataObject secretData
+	var secretCompleteObject SecretYaml
 	var secretString strings.Builder
 	scanner := bufio.NewScanner(os.Stdin)
 	if scanner.Err() != nil {
@@ -30,26 +33,53 @@ func readObject() (yData *secretData, yComplete Secret, objectType string){
 	for scanner.Scan() {
 		secretString.WriteString(scanner.Text()+"\n")
 	}
-	// only data part
-	err := yaml.Unmarshal([]byte(secretString.String()),&yamlData)
-	if err != nil {
-		fmt.Printf("Failed to decode yaml %v\n",err)
+
+	// check if data is json
+	OutputType = isJson(secretString.String())
+
+	if OutputType == "json" {
+		// only data part
+		err := json.Unmarshal([]byte(secretString.String()),&secretDataObject)
+		if err != nil {
+			fmt.Printf("Failed to decode object %v\n",err)
+		}
+		// complete json object
+		err = json.Unmarshal([]byte(secretString.String()),&secretCompleteObject)
+		if err != nil {
+			fmt.Printf("Failed to decode object %v\n",err)
+		}
+
+	} else {
+		// only data part
+		err := yaml.Unmarshal([]byte(secretString.String()),&secretDataObject)
+		if err != nil {
+			fmt.Printf("Failed to decode object %v\n",err)
+		}
+		// complete yaml object
+		err = yaml.Unmarshal([]byte(secretString.String()),&secretCompleteObject)
+		if err != nil {
+			fmt.Printf("Failed to decode object %v\n",err)
+		}
 	}
-	// complete yaml object
-	err = yaml.Unmarshal([]byte(secretString.String()),&yamlComplete)
-	if err != nil {
-		fmt.Printf("Failed to decode yaml %v\n",err)
-	}
-	if !isSecretObject(&yamlData) {
+
+	if !isSecretObject(&secretDataObject) {
 		fmt.Println("The given object is not a Secret object")
 		return
 	}
-	return &yamlData, yamlComplete, strings.ToLower(yamlData.Type)
+	return &secretDataObject, secretCompleteObject, strings.ToLower(secretDataObject.Type)
 }
 
 // Validate the secret object kind
 func isSecretObject(y *secretData) bool {
 	return strings.ToLower(y.Kind) == "secret"
+}
+
+func isJson(s string) string{
+	var js interface{}
+	if json.Unmarshal([]byte(s),&js) == nil {
+		return "json"
+	}
+	return "nil"
 }
 
 // Identify the secret object from following types:
